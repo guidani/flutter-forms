496e3936a3bc1ccda8404a5b93317f3377a4cdf7
guilherme <gui.dani13@gmail.com>
2022-05-13 09:00:32 -0300

    criado questão 4 - ligar e desligar luz

--- lib/pages/light/light_page.dart
+++ lib/pages/light/light_page.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 
 class LightPage extends StatefulWidget {
-  const LightPage({ Key? key }) : super(key: key);
+  const LightPage({Key? key}) : super(key: key);
 
   @override
   State<LightPage> createState() => _LightPageState();
 }
 
 class _LightPageState extends State<LightPage> {
+  bool value = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,11 +19,52 @@ class _LightPageState extends State<LightPage> {
       body: Padding(
         padding: EdgeInsets.all(10.0),
         child: Center(
+          child: Expanded(
             child: Column(
-          children: [
-            Text('asd'),
-          ],
-        )),
+              children: [
+                value == true ? lightOn() : lightOff(),
+                const SizedBox(
+                  height: 10.0,
+                ),
+                Text('Ligar/Desligar'),
+                Switch.adaptive(
+                    value: value,
+                    onChanged: (value) {
+                      setState(() => {
+                            this.value = value,
+                          });
+                    }),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget lightOff() {
+    return Container(
+      width: 100.0,
+      height: 100.0,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        border: Border.all(),
+        color: Colors.white70,
+      ),
+    );
+  }
+
+  Widget lightOn() {
+    return Container(
+      width: 100.0,
+      height: 100.0,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        border: Border.all(),
+        color: Colors.yellow,
+        boxShadow: [
+          BoxShadow(color: Colors.yellow, spreadRadius: 10.0, blurRadius: 10.0)
+        ],
       ),
     );
   }
