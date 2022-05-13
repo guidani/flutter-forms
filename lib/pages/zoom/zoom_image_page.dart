7044c0417664214a0033331738674487572851da
guilherme <gui.dani13@gmail.com>
2022-05-13 09:31:24 -0300

    criado página de zoom - ainda imcompleto

--- lib/pages/zoom/zoom_image_page.dart
+++ lib/pages/zoom/zoom_image_page.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 
 class ZoomImage extends StatefulWidget {
-  const ZoomImage({ Key? key }) : super(key: key);
+  const ZoomImage({Key? key}) : super(key: key);
 
   @override
   State<ZoomImage> createState() => _ZoomImageState();
 }
 
 class _ZoomImageState extends State<ZoomImage> {
+  double _currentSliderValue = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,12 +17,42 @@ class _ZoomImageState extends State<ZoomImage> {
         centerTitle: true,
       ),
       body: Padding(
-        padding: EdgeInsets.all(10.0),
+        padding: const EdgeInsets.all(10.0),
         child: Center(
-            child: Column(
-          children: [
-            Text('asd'),
-          ],
+            child: Expanded(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Spacer(),
+              Container(
+                width: 300.0,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    style: BorderStyle.solid,
+                    color: Colors.amber,
+                    width: 5.0,
+                  ),
+                ),
+                child: Image.asset(
+                  'assets/images/imagem01.png',
+                  // scale: 2,
+                  // fit: BoxFit.cover,
+                ),
+              ),
+              Spacer(),
+              Slider(
+                  value: _currentSliderValue,
+                  max: 10,
+                  // divisions: 10,
+                  label: _currentSliderValue.round().toString(),
+                  onChanged: (double value) {
+                    setState(() {
+                      _currentSliderValue = value;
+                    });
+                  })
+            ],
+          ),
         )),
       ),
     );
