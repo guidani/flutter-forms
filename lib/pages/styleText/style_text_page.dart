29ca9b3ae232905f23b5127141618055490f4bf1
guilherme <gui.dani13@gmail.com>
2022-05-17 09:28:35 -0300

    changes

--- lib/pages/styleText/style_text_page.dart
+++ lib/pages/styleText/style_text_page.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-
-String loremText =
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In facilisis auctor laoreet. Aenean varius fringilla dolor facilisis efficitur. Maecenas eleifend laoreet leo, ut faucibus urna tempus ut. Mauris ullamcorper et enim a pulvinar. Nulla molestie tortor et imperdiet luctus. Maecenas vel libero at ipsum condimentum egestas eget vel enim. Integer ut orci vitae sem auctor iaculis non id lectus. Pellentesque lobortis, libero id tempus dignissim, urna ex dapibus ante, sed tincidunt justo orci ut metus. Donec at ante laoreet dolor pharetra hendrerit id vel nisl.Lorem ipsum dolor sit amet, consectetur adipiscing elit. In facilisis auctor laoreet. Aenean varius fringilla dolor facilisis efficitur. Maecenas eleifend laoreet leo, ut faucibus urna tempus ut. Mauris ullamcorper et enim a pulvinar. Nulla molestie tortor et imperdiet luctus. Maecenas vel libero at ipsum condimentum egestas eget vel enim. Integer ut orci vitae sem auctor iaculis non id lectus. Pellentesque lobortis, libero id tempus dignissim, urna ex dapibus ante, sed tincidunt justo orci ut metus. Donec at ante laoreet dolor pharetra hendrerit id vel nisl.Lorem ipsum dolor sit amet, consectetur adipiscing elit. In facilisis auctor laoreet. Aenean varius fringilla dolor facilisis efficitur. Maecenas eleifend laoreet leo, ut faucibus urna tempus ut. Mauris ullamcorper et enim a pulvinar. Nulla molestie tortor et imperdiet luctus. Maecenas vel libero at ipsum condimentum egestas eget vel enim. Integer ut orci vitae sem auctor iaculis non id lectus. Pellentesque lobortis, libero id tempus dignissim, urna ex dapibus ante, sed tincidunt justo orci ut metus. Donec at ante laoreet dolor pharetra hendrerit id vel nisl.';
+import 'package:forms/pages/styleText/mock_text.dart';
 
 class StyleTextPage extends StatefulWidget {
   const StyleTextPage({Key? key}) : super(key: key);
@@ -9,6 +7,8 @@ class StyleTextPage extends StatefulWidget {
   @override
   State<StyleTextPage> createState() => _StyleTextPageState();
 }
+
+enum PersonGender { masculino, feminino, outro }
 
 class _StyleTextPageState extends State<StyleTextPage> {
   final _formKey = GlobalKey<FormState>();
@@ -26,6 +26,8 @@ class _StyleTextPageState extends State<StyleTextPage> {
     "isUnderline": false
   };
 
+  PersonGender? _personGender = PersonGender.masculino;
+
   bool _telemetryCheckbox = false;
   bool _promotionCheckbox = false;
   bool _serviceTermsCheckbox = false;
@@ -35,21 +37,18 @@ class _StyleTextPageState extends State<StyleTextPage> {
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     myFocusNode = FocusNode();
   }
 
   @override
   void dispose() {
-    // TODO: implement dispose
     super.dispose();
     myFocusNode.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    print(decorations.keys);
     return Scaffold(
       resizeToAvoidBottomInset: false,
       appBar: AppBar(
@@ -60,96 +59,162 @@ class _StyleTextPageState extends State<StyleTextPage> {
           padding: const EdgeInsets.all(8.0),
           child: Form(
             key: _formKey,
-            child: Column(
-              children: [
-                TextFormField(
-                  onChanged: (value) {},
-                  controller: _nameController,
-                  textInputAction: TextInputAction.next,
-                  autofocus: true,
-                  focusNode: myFocusNode,
-                  validator: (text) {
-                    if (text == null || text.isEmpty) {
-                      return 'Este campo precisa ser preenchido';
-                    }
-                    return null;
-                  },
-                  decoration: const InputDecoration(
-                    label: Text('Nome'),
-                    hintText: 'Digite seu nome',
-                    border: OutlineInputBorder(),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  const Text(
+                    'Cadastro',
+                    style: TextStyle(fontSize: 30),
                   ),
-                ),
-                const SizedBox(
-                  height: 10.0,
-                ),
-                TextFormField(
-                  controller: _lastNameController,
-                  textInputAction: TextInputAction.next,
-                  validator: (text) {
-                    if (text == null || text.isEmpty) {
-                      return 'Este campo precisa ser preenchido';
-                    }
-                    return null;
-                  },
-                  decoration: const InputDecoration(
-                    label: Text('Sobrenome'),
-                    hintText: 'Digite seu sobrenome',
-                    border: OutlineInputBorder(),
+                  const Divider(),
+                  TextFormField(
+                    onChanged: (value) {},
+                    controller: _nameController,
+                    textInputAction: TextInputAction.next,
+                    autofocus: true,
+                    focusNode: myFocusNode,
+                    validator: (text) {
+                      if (text == null || text.isEmpty) {
+                        return 'Este campo precisa ser preenchido';
+                      }
+                      return null;
+                    },
+                    decoration: const InputDecoration(
+                      label: Text('Nome'),
+                      hintText: 'Digite seu nome',
+                      border: OutlineInputBorder(),
+                    ),
                   ),
-                ),
-                const SizedBox(
-                  height: 10.0,
-                ),
-                TextFormField(
-                  controller: _descriptionController,
-                  validator: (text) {
-                    if (text == null || text.isEmpty) {
-                      return 'Este campo precisa ser preenchido';
-                    }
-                    return null;
-                  },
-                  style: TextStyle(
-                    fontStyle: isItalic ? FontStyle.italic : null,
-                    fontWeight: isBold ? FontWeight.bold : null,
-                    decoration: isUnderline ? TextDecoration.underline : null,
+                  const SizedBox(
+                    height: 10.0,
                   ),
-                  decoration: const InputDecoration(
-                    hintText: 'Fale um pouco sobre você',
-                    border: OutlineInputBorder(),
+                  TextFormField(
+                    controller: _lastNameController,
+                    textInputAction: TextInputAction.next,
+                    validator: (text) {
+                      if (text == null || text.isEmpty) {
+                        return 'Este campo precisa ser preenchido';
+                      }
+                      return null;
+                    },
+                    decoration: const InputDecoration(
+                      label: Text('Sobrenome'),
+                      hintText: 'Digite seu sobrenome',
+                      border: OutlineInputBorder(),
+                    ),
                   ),
-                ),
-                const SizedBox(
-                  height: 10.0,
-                ),
-                ToggleButtons(
-                  children: const [
-                    Icon(Icons.format_italic),
-                    Icon(Icons.format_bold),
-                    Icon(Icons.format_underline),
-                  ],
-                  isSelected: _selections,
-                  color: Colors.grey,
-                  selectedColor: Colors.black,
-                  onPressed: (int index) {
-                    setState(() {
-                      _selections[index] = !_selections[index];
-                      isBold = _selections[0];
-                      isItalic = _selections[1];
-                      isUnderline = _selections[2];
-                    });
-                  },
-                ),
-                const SizedBox(
-                  height: 10.0,
-                ),
-                Expanded(
-                  child: Container(
+// ! NÃO FUNCIONANDO
+                  InputDatePickerFormField(
+                    errorFormatText: 'Formato de data incorreta',
+                    errorInvalidText: 'Data inválida',
+                    fieldHintText: 'dd/mm/YYYY',
+                    fieldLabelText: 'Data de nascimento',
+                    // initialDate: DateTime.now(),
+                    firstDate: DateTime(1900),
+                    lastDate: DateTime(2999),
+                  ),
+                  const SizedBox(
+                    height: 10.0,
+                  ),
+                  const SizedBox(
+                    height: 10.0,
+                  ),
+                  const Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'Sexo: ',
+                      style: TextStyle(fontSize: 15),
+                    ),
+                  ),
+                  ListTile(
+                    title: const Text('Masculino'),
+                    leading: Radio<PersonGender>(
+                      value: PersonGender.masculino,
+                      groupValue: _personGender,
+                      onChanged: (PersonGender? value) {
+                        setState(() {
+                          _personGender = value;
+                        });
+                      },
+                    ),
+                  ),
+                  ListTile(
+                    title: const Text('Feminino'),
+                    leading: Radio<PersonGender>(
+                      value: PersonGender.feminino,
+                      groupValue: _personGender,
+                      onChanged: (PersonGender? value) {
+                        setState(() {
+                          _personGender = value;
+                        });
+                      },
+                    ),
+                  ),
+                  ListTile(
+                    title: const Text('Outro'),
+                    leading: Radio<PersonGender>(
+                      value: PersonGender.outro,
+                      groupValue: _personGender,
+                      onChanged: (PersonGender? value) {
+                        setState(() {
+                          _personGender = value;
+                        });
+                      },
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10.0,
+                  ),
+                  TextFormField(
+                    controller: _descriptionController,
+                    validator: (text) {
+                      if (text == null || text.isEmpty) {
+                        return 'Este campo precisa ser preenchido';
+                      }
+                      return null;
+                    },
+                    style: TextStyle(
+                      fontStyle: isItalic ? FontStyle.italic : null,
+                      fontWeight: isBold ? FontWeight.bold : null,
+                      decoration: isUnderline ? TextDecoration.underline : null,
+                    ),
+                    decoration: const InputDecoration(
+                      label: Text('Descrição'),
+                      hintText: 'Fale um pouco sobre você',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10.0,
+                  ),
+                  ToggleButtons(
+                    children: const [
+                      Icon(Icons.format_italic),
+                      Icon(Icons.format_bold),
+                      Icon(Icons.format_underline),
+                    ],
+                    isSelected: _selections,
+                    color: Colors.grey,
+                    selectedColor: Colors.black,
+                    onPressed: (int index) {
+                      setState(() {
+                        _selections[index] = !_selections[index];
+                        isBold = _selections[0];
+                        isItalic = _selections[1];
+                        isUnderline = _selections[2];
+                      });
+                    },
+                  ),
+                  const SizedBox(
+                    height: 10.0,
+                  ),
+                  Container(
+                    height: 200,
                     decoration: BoxDecoration(
                       border: Border.all(color: Colors.black, width: 1.0),
                     ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(10.0),
+                    child: const Padding(
+                      padding: EdgeInsets.all(10.0),
                       child: SingleChildScrollView(
                         child: Text(
                           loremText,
@@ -158,85 +223,72 @@ class _StyleTextPageState extends State<StyleTextPage> {
                       ),
                     ),
                   ),
-                ),
-                const SizedBox(
-                  height: 10.0,
-                ),
-                Expanded(
-                  child: ListView(
-                    children: [
-                      ListTile(
-                        leading: Checkbox(
-                          value: _telemetryCheckbox,
-                          onChanged: (value) {
-                            setState(() {
-                              _telemetryCheckbox = value!;
-                            });
-                          },
-                        ),
-                        title: const Text(
-                            'Aceito compartilhar dados para melhoria do aplicativo.”'),
-                      ),
-                      ListTile(
-                        leading: Checkbox(
-                          value: _promotionCheckbox,
-                          onChanged: (value) {
-                            setState(() {
-                              _promotionCheckbox = value!;
-                            });
-                          },
-                        ),
-                        title: const Text(
-                            'Aceito receber comunicação sobre promoções.”'),
-                      ),
-                      ListTile(
-                        leading: Checkbox(
-                          value: _serviceTermsCheckbox,
-                          onChanged: (value) {
-                            setState(() {
-                              _serviceTermsCheckbox = value!;
-                            });
-                          },
-                        ),
-                        title: const Text('Aceito os termos de serviço.”'),
-                      ),
-                      ListTile(
-                        leading: Checkbox(
-                          value: _acceptAllTerms,
-                          onChanged: (value) {
-                            setState(() {
-                              _acceptAllTerms = value!;
-                              _serviceTermsCheckbox = value;
-                              _promotionCheckbox = value;
-                              _telemetryCheckbox = value;
-                            });
-                          },
-                        ),
-                        title: const Text('Aceitar todos os items acima.”'),
-                      ),
-                    ],
+                  const SizedBox(
+                    height: 10.0,
                   ),
-                ),
-                const SizedBox(
-                  height: 10.0,
-                ),
-                SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      if (_formKey.currentState!.validate()) {
-                        _formKey.currentState!.save();
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(
-                            content: Text('Enviando dados!'),
-                          ),
-                        );
-                      }
+                  CheckboxListTile(
+                    title: const Text(
+                        'Aceito compartilhar dados para melhoria do aplicativo.'),
+                    value: _telemetryCheckbox,
+                    onChanged: (value) {
+                      setState(() {
+                        _telemetryCheckbox = value!;
+                      });
                     },
-                    child: const Text('Enviar'),
                   ),
-                )
-              ],
+                  CheckboxListTile(
+                    title: const Text(
+                        'Aceito receber comunicação sobre promoções.”'),
+                    value: _promotionCheckbox,
+                    onChanged: (value) {
+                      setState(() {
+                        _promotionCheckbox = value!;
+                      });
+                    },
+                  ),
+                  CheckboxListTile(
+                    title: const Text('Aceito os termos de serviço.'),
+                    value: _serviceTermsCheckbox,
+                    onChanged: (value) {
+                      setState(() {
+                        _serviceTermsCheckbox = value!;
+                      });
+                    },
+                  ),
+                  CheckboxListTile(
+                    title: const Text('Aceitar todos os items acima.'),
+                    value: _acceptAllTerms,
+                    onChanged: (value) {
+                      setState(() {
+                        _acceptAllTerms = value!;
+                        _serviceTermsCheckbox = value;
+                        _promotionCheckbox = value;
+                        _telemetryCheckbox = value;
+                      });
+                    },
+                  ),
+                  const SizedBox(
+                    height: 10.0,
+                  ),
+                  SizedBox(
+                    height: 50,
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          _formKey.currentState!.save();
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content: Text('Enviando dados!'),
+                            ),
+                          );
+                        }
+                      },
+                      child: const Text('Enviar'),
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ),
