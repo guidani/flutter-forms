76c2121e7de937750534c9f344361d35e3951f10
guilherme <gui.dani13@gmail.com>
2022-05-18 19:55:36 -0300

    finalizado

---
+++ lib/pages/filter-api/api_service.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+
+import 'package:forms/pages/filter-api/user_model.dart';
+import 'package:http/http.dart' as http;
+
+class FetchUser {
+  var data = [];
+  List<User> results = [];
+  String urlList = 'https://jsonplaceholder.typicode.com/users/';
+
+  Future<List<User>> getuser({String? query}) async {
+    var url = Uri.parse(urlList);
+    try {
+      var response = await http.get(url);
+      if (response.statusCode == 200) {
+        data = json.decode(response.body);
+        results = data.map((e) => User.fromJson(e)).toList();
+        if (query != null) {
+          results = results
+              .where((element) =>
+                  element.name!.toLowerCase().contains((query.toLowerCase())))
+              .toList();
+        }
+      } else {
+        print("fetch error");
+      }
+    } on Exception catch (e) {
+      print('error: $e');
+    }
+    return results;
+  }
+}
--- lib/pages/filter-api/filter_api_list_page.dart
+++ lib/pages/filter-api/filter_api_list_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:forms/pages/filter-api/api_service.dart';
+import 'package:forms/pages/filter-api/search.dart';
+import 'package:forms/pages/filter-api/user_model.dart';
 
 class FilterApiList extends StatefulWidget {
   const FilterApiList({Key? key}) : super(key: key);
@@ -8,21 +11,88 @@ class FilterApiList extends StatefulWidget {
 }
 
 class _FilterApiListState extends State<FilterApiList> {
+  FetchUser _user = FetchUser();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Filter From API'),
-        centerTitle: true,
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(10.0),
-        child: Center(
-            child: Column(
-          children: [
-            Text('asd'),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('UserList'),
+          actions: [
+            IconButton(
+              onPressed: () {
+                showSearch(context: context, delegate: SearchUser());
+              },
+              icon: Icon(Icons.search_sharp),
+            )
           ],
-        )),
+        ),
+        body: Container(
+          padding: EdgeInsets.all(20),
+          child: FutureBuilder<List<User>>(
+              future: _user.getuser(),
+              builder: (context, snapshot) {
+                var data = snapshot.data;
+                return ListView.builder(
+                    itemCount: data?.length,
+                    itemBuilder: (context, index) {
+                      if (!snapshot.hasData) {
+                        return Center(child: CircularProgressIndicator());
+                      }
+                      return Card(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: ListTile(
+                            title: Row(
+                              children: [
+                                Container(
+                                  width: 60,
+                                  height: 60,
+                                  decoration: BoxDecoration(
+                                    color: Colors.deepPurpleAccent,
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                  child: Center(
+                                    child: Text(
+                                      '${data?[index].id}',
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white),
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(width: 20),
+                                Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        '${data?[index].name}',
+                                        style: TextStyle(
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.w600),
+                                      ),
+                                      SizedBox(height: 10),
+                                      Text(
+                                        '${data?[index].email}',
+                                        style: TextStyle(
+                                          color: Colors.black,
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.w400,
+                                        ),
+                                      ),
+                                    ])
+                              ],
+                            ),
+                            // trailing: Text('More Info'),
+                          ),
+                        ),
+                      );
+                    });
+              }),
+        ),
       ),
     );
   }
