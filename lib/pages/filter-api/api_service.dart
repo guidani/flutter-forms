5b19eff89b552ff605b87bb9799b9313f32213f3
guilherme <gui.dani13@gmail.com>
2022-05-19 07:31:12 -0300

    fix

--- lib/pages/filter-api/api_service.dart
+++ lib/pages/filter-api/api_service.dart
@@ -1,7 +1,8 @@
 import 'dart:convert';
 
-import 'package:forms/pages/filter-api/user_model.dart';
 import 'package:http/http.dart' as http;
+
+import 'package:forms/pages/filter-api/user_model.dart';
 
 class FetchUser {
   var data = [];
